431d6a44d8e639efc2b2de8a36c2ca254540c7bd
vvarma2020 <106948726+vvarma2020@users.noreply.github.com>
2022-10-12 20:13:00 +0530

    web search api added

---
+++ controllers/searchQuery.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+/*
+This code uses serpstack custom search API. For more information
+refer their API documentation : https://serpstack.com/documentation#example_php
+Configure the access key using a .env file.
+*/
+
+const searchQuery = async (query) => {
+    const params = {
+        'access_key': process.env.SERPSTACK_ACCESS_KEY,
+        'query': query // this is where the query string will be set
+        // 'type': 'shopping' // specify type for search
+    }
+
+    await axios.get('https://api.serpstack.com/search', { params })
+        .then(response => {
+            const apiResponse = response.data;
+            console.log("Total results: ", apiResponse.search_information.total_results);
+            apiResponse.organic_results.map((result, number) =>
+                console.log(`${number + 1}, ${result.title}`));
+        }).catch(error => {
+            console.log(err);
+        });
+}
+
+module.exports = searchQuery;
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+require('dotenv').config(); // get local .env variables into process.env
 const express = require('express');
 const app = express();
 const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
 
 // Custom logger middleware.
 app.use(logger);
 
-console.log('testing...');
+// Serve static files for root
+app.use(express.static('./public'));
+
+// console.log(process.env.SERPSTACK_ACCESS_KEY);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))
